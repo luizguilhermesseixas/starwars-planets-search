b402027a58936144916208478be1345a3ebac7f7
Luiz Guilherme S Seixas <guilhermesschiavon@gmail.com>
2023-03-29 17:56:12 -0300

    Realiza requisito 3

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import fetchApi from '../services/fetchApi';
 
@@ -8,6 +8,9 @@ function AppProvider({ children }) {
   const [apiData, setApiData] = useState([]);
   const [originalData, setOriginalData] = useState([]);
   const [filterByName, setFilterByName] = useState('');
+  const [columnFilter, setColumnFilter] = useState('');
+  const [comparisonFilter, setComparisonFilter] = useState('');
+  const [valueFilter, setValueFilter] = useState(null);
 
   useEffect(() => {
     fetchApi().then((data) => {
@@ -26,10 +29,41 @@ function AppProvider({ children }) {
     }
   }, [filterByName, originalData]);
 
+  const comparisonToString = useCallback((param) => {
+    const parseValueFilter = Number(valueFilter);
+    const parseColumnItem = Number(param[columnFilter]);
+    if (comparisonFilter === '>') {
+      return parseColumnItem > parseValueFilter;
+    }
+    if (comparisonFilter === '<') {
+      return parseColumnItem < parseValueFilter;
+    }
+    if (comparisonFilter === '===') {
+      return parseColumnItem === parseValueFilter;
+    }
+  }, [comparisonFilter, columnFilter, valueFilter]);
+
+  useEffect(() => {
+    if (valueFilter !== null) {
+      setApiData(
+        originalData
+          .filter((eachPlanet) => comparisonToString(eachPlanet)),
+      );
+    } else {
+      setApiData(originalData);
+    }
+  }, [columnFilter, comparisonFilter, valueFilter, originalData, comparisonToString]);
+
   const values = {
     apiData,
     filterByName,
     setFilterByName,
+    columnFilter,
+    setColumnFilter,
+    comparisonFilter,
+    setComparisonFilter,
+    valueFilter,
+    setValueFilter,
   };
 
   return (
--- src/pages/Filters.js
+++ src/pages/Filters.js
@@ -1,13 +1,55 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import AppContext from '../context/AppContext';
 
 function Filters() {
-  const { filterByName, setFilterByName } = useContext(AppContext);
+  const [localComparison, setLocalComparison] = useState('maior que');
+  const [localColumn, setLocalColumn] = useState('population');
+  const [localValue, setLocalValue] = useState(0);
+  const {
+    filterByName,
+    setFilterByName,
+    /*     columnFilter, */
+    setColumnFilter,
+    /*     comparisonFilter, */
+    setComparisonFilter,
+    /*     valueFilter, */
+    setValueFilter,
+  } = useContext(AppContext);
 
   const inputTextChange = ({ target: { value } }) => {
     setFilterByName(
       value,
     );
+  };
+
+  const inputColumnChange = ({ target: { value } }) => {
+    setLocalColumn(
+      value,
+    );
+  };
+
+  const inputComparisonChange = ({ target: { value } }) => {
+    setLocalComparison(
+      value,
+    );
+  };
+
+  const inputValueChange = ({ target: { value } }) => {
+    setLocalValue(
+      value,
+    );
+  };
+
+  const handleClick = () => {
+    if (localComparison === 'maior que') {
+      setComparisonFilter('>');
+    } if (localComparison === 'menor que') {
+      setComparisonFilter('<');
+    } if (localComparison === 'igual a') {
+      setComparisonFilter('===');
+    }
+    setColumnFilter(localColumn);
+    setValueFilter(localValue);
   };
 
   return (
@@ -20,6 +62,38 @@ function Filters() {
         type="text"
         data-testid="name-filter"
       />
+      <select
+        data-testid="column-filter"
+        value={ localColumn }
+        onChange={ inputColumnChange }
+      >
+        <option>population</option>
+        <option>orbital_period</option>
+        <option>diameter</option>
+        <option>rotation_period</option>
+        <option>surface_water</option>
+      </select>
+      <select
+        data-testid="comparison-filter"
+        value={ localComparison }
+        onChange={ inputComparisonChange }
+      >
+        <option>maior que</option>
+        <option>menor que</option>
+        <option>igual a</option>
+      </select>
+      <input
+        type="number"
+        data-testid="value-filter"
+        value={ localValue }
+        onChange={ inputValueChange }
+      />
+      <button
+        data-testid="button-filter"
+        onClick={ handleClick }
+      >
+        Aplicar filtro
+      </button>
     </header>
   );
 }
